ecc80912480113b36caa0691c8b52ac597b2eac2
fetchingg <nastya.zhdanova.2009@gmail.com>
2026-03-09 13:51:27 +0300

    Initial commit

---
+++ project3/ice1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ice1" tilewidth="70" tileheight="70" tilecount="98" columns="14">
+ <image source="sheet.png" width="980" height="490"/>
+</tileset>
